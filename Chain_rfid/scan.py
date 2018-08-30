78c91018429621f3692d4bf33f7e9782c5c2f7b5
Lebski <email@felixleber.de>
2018-08-30 16:39:49 +0200

    fixed brackets

--- Chain_rfid/scan.py
+++ Chain_rfid/scan.py
@@ -67,7 +67,7 @@ while continue_reading:
                 #uid_str = ', '.join('"' + str(i) +  '"' for i in uid)
                 #in_Data_str = ', '.join('"' + str(i) +  '"' for i in in_Data)
 
-                json_str = '{"uid": [' + str(uid) + '], "payload": [' + str(in_Data) + ']}'
+                json_str = '{"uid": ' + str(uid) + ', "payload": ' + str(in_Data) + '}'
                 sender.send_message(json_str)
             MIFAREReader.MFRC522_StopCrypto1()
         else:
