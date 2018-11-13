16d47fa67a79f4ad16506b50baa310252512abc7
Lebski <email@felixleber.de>
2018-11-13 10:11:57 +0100

    Edited interface, restructured

--- 01_Webserver/router.py
+++ 01_Webserver/router.py
@@ -1,12 +1,14 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def show_index():
+    return redirect("/static/index.html", code=302)
 
 
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % username
-
-#url_for('static', filename='intex.html')
-#url_for('static', filename='style.css')
